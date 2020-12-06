fd2f6e4fa5115c8146e13842b6708cd7847c31a0
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2020-12-06 09:16:52 -0500

    made both directions rc-update

---
+++ rc-pull.sh
@@ -0,0 +1,4 @@
+cp ~/wsl-setup/auxiliary-files/.zsh_aliases ~/
+cp ~/wsl-setup/auxiliary-files/UltiSnips ~/.vim/ -r
+cp ~/wsl-setup/auxiliary-files/ftplugin ~/.vim/ -r
+cp ~/wsl-setup/auxiliary-files/.vimrc ~/
