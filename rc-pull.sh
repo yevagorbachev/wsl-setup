967fd7c941a2ed2c65fd9f2b9b63487b946bb0f0
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2021-07-26 10:18:04 -0400

    Fixed pull/push files

--- rc-pull.sh
+++ rc-pull.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 read -p "Copy configuration files from repo to system [y/n]: " -n 1 REPLY
 echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+if [[ $REPLY =~ ^[Yy]$ ]]; then
 	cp ~/wsl-setup/auxiliary-files/.zsh_aliases ~/
 	cp ~/wsl-setup/auxiliary-files/UltiSnips ~/vim/ -r
 	cp ~/wsl-setup/auxiliary-files/ftplugin ~/.vim/ -r
--- rc-push.sh
+++ rc-push.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 read -p "Copy configuration files from system to repo? [y/n]: " -n 1 REPLY
 echo
-if [[ $REPLY =~ ^[Yy]$ ]];
-then
+if [[ $REPLY =~ '^[Yy]$' ]]; then
 	cp ~/.vim/ftplugin ~/wsl-setup/auxiliary-files/ -r
 	cp ~/.vim/UltiSnips ~/wsl-setup/auxiliary-files/ -r
 	cp ~/.zsh_aliases ~/wsl-setup/auxiliary-files
