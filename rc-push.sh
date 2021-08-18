f4125f86961efb3da1e5834ffd4bc100f2d3b451
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2021-08-18 11:17:31 -0400

    fixed rc pull/push

--- rc-push.sh
+++ rc-push.sh
@@ -2,8 +2,8 @@
 read -p "Copy configuration files from system to repo? [y/n]: " -n 1 REPLY
 echo
 if [[ $REPLY =~ '^[Yy]$' ]]; then
-	cp ~/.vim/ftplugin ~/wsl-setup/auxiliary-files/ -r
-	cp ~/.vim/UltiSnips ~/wsl-setup/auxiliary-files/ -r
-	cp ~/.zsh_aliases ~/wsl-setup/auxiliary-files
-	cp ~/.vimrc ~/wsl-setup/auxiliary-files
+	cp ~/.zsh_aliases ~/wsl-setup/configs/
+	cp ~/.vimrc ~/wsl-setup/configs/
+	cp ~/.vim/UltiSnips ~/wsl-setup/configs/.vim -r
+	cp ~/.vim/ftplugin ~/wsl-setup/configs/.vim -r
 fi
