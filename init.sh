82964c49e20626af41cd42257e95305d519195f7
Yevgeniy Gorbachev <yevgeniy.gorbachev@gmail.com>
2022-02-28 05:53:33 -0800

    Updated init.sh to reflect directory change

--- init.sh
+++ init.sh
@@ -17,20 +17,9 @@ chsh -s /bin/zsh
 sed 's/ZSH_THEME=\"[a-z]*\"/ZSH_THEME=\"afowler-custom\"/g' ~/.zshrc -i # set ZSH theme
 cp ~/wsl-setup/auxiliary-files/afowler-custom.zsh-theme ~/.oh-my-zsh/themes/ # update with my custom theme
 
-# set up home dir
-cp ~/wsl-setup/configs/* ~/ -r
-
+# copy configuration files
+cp ~/wsl-setup/configs/. ~/ -r
 # add aliases
 echo '. ~/.zsh_aliases' >> ~/.zshrc
-
 # VimPlug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-cp ~/wsl-setup/auxiliary-files/.vimrc ~/
-# Copy 
-
-cp ~/wsl-setup/auxiliary-files/UltiSnips ~/.vim/ -r
-cp ~/wsl-setup/auxiliary-files/ftplugin ~/.vim/ -r
-
-# touch ~/.toggl_token
-# touch ~/.toggl_wid
-# echo "REMEMBER TO UPDATE ~/.toggl_token"
