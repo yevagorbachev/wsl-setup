b696cc0ade5ae6da1dc75774820162022628ace6
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2022-03-22 12:46:21 -0400

    fixed syntax in rc-push

--- rc-push.sh
+++ rc-push.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 read -p "Copy configuration files from system to repo? [y/n]: " -n 1 REPLY
 echo
-if [[ $REPLY =~ '^[Yy]$' ]]; then
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	echo "Copying ~/.zsh_aliases"
 	cp ~/.zsh_aliases ~/wsl-setup/configs/
+	echo "Copying ~/.vimrc"
 	cp ~/.vimrc ~/wsl-setup/configs/
+	echo "Copying select ./vim subdirs"
 	cp ~/.vim/UltiSnips ~/wsl-setup/configs/.vim -r
 	cp ~/.vim/ftplugin ~/wsl-setup/configs/.vim -r
 fi
