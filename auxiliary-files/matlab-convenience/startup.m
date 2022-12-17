2ff9662d2c9a29ea9d540018b4836cb237b4d663
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2022-12-17 14:37:13 -0500

    Added movingslope and startup .m

---
+++ auxiliary-files/matlab-convenience/startup.m
@@ -0,0 +1,6 @@
+set(0, 'DefaultTextInterpreter', 'latex');
+set(0, 'DefaultLegendInterpreter', 'latex');
+set(0, 'DefaultAxesTickLabelInterpreter', 'latex');
+set(0, 'DefaultFigureWindowStyle', 'docked');
+
+
