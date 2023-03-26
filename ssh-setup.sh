4fc9880e32840b6361ce4ca5e71117afbeb6e250
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2023-03-26 11:31:17 -0400

    Added to git config statements

--- ssh-setup.sh
+++ ssh-setup.sh
@@ -7,6 +7,8 @@ cat ~/.ssh/id_rsa.pub
 # configure GitHub details
 git config --global user.email $2
 git config --global user.name $3
+git config --global core.editor vim
 git config --global diff.tool vimdiff
 git config --global difftool.prompt false
+# git config --global merge.tool vimdiff
 
